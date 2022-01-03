b65f230d6c4310692164cfbbc90471a1a37d5686
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-01-03 13:23:00 -0500

    Funcion de mostrar interfaz escanear qr al hacer click
    creamos una funcion toggle para que se despkiegue y pliegue la div_escanner_QR , cuando hacemos click en ella.

--- js/QR.js
+++ js/QR.js
@@ -1,5 +1,19 @@
 var boton_QR_icono = document.querySelector("#icono-QR");
+var div_escanner_QR = document.querySelector(".div-escanear-QR");
 
 boton_QR_icono.addEventListener("click", function(){               
-     alert("DIMOS CLIKCK EN ICONO QR");
+    //alert("DIMOS CLIKCK EN ICONO QR");
+    $(".div-escanear-QR").toggleClass("mostrar-interfaz-QR");    
+    
  });
+
+ div_escanner_QR.addEventListener("click", function(){
+    $(".div-escanear-QR").toggleClass("mostrar-interfaz-QR");    
+ });
+
+ /*
+ $(document).on('click', '.btn-ver' , function(){                
+    //despliegue detalles con toggle          
+    $(".div-detalles-carrito").toggleClass("mostrar-detalles-carrito");        
+}); 
+*/
