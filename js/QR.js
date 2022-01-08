ce7877d68365b7a5f3bccb7ef511659699cb5684
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-01-08 11:50:36 -0500

    cuando entramos a interfaz de escanear qr se desactiva el scroll
    es decir no se puede desplazar hacia abajo en la pagina ya que , se desactiva  el scroll

--- js/QR.js
+++ js/QR.js
@@ -1,13 +1,20 @@
 var boton_QR_icono = document.querySelector("#icono-QR");
 var div_escanner_QR = document.querySelector(".div-escanear-QR");
 
+var  bandera_dentro_interfaz = false;
+
 boton_QR_icono.addEventListener("click", function(){               
     //alert("DIMOS CLIKCK EN ICONO QR");
+    bandera_dentro_interfaz = true;
+    //desactivamos el desplazamiento vertical con la propiedad overflow-y: hidden de la etiqueta html    
+    $("html").css("overflow-y", "hidden");
     $(".div-escanear-QR").toggleClass("mostrar-interfaz-QR");    
     
  });
 
  div_escanner_QR.addEventListener("click", function(){
+   bandera_dentro_interfaz = false;
+   $("html").css("overflow-y", "scroll");
     $(".div-escanear-QR").toggleClass("mostrar-interfaz-QR");    
  });
 
