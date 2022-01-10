82bef73c5bbced3f87ba7812504a6b92ea536b92
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-01-10 00:58:08 -0500

    Generando QR en una hora determinada

--- js/QR.js
+++ js/QR.js
@@ -13,18 +13,66 @@ const  run = async() => {
 }
 run()
 */
+ var  horaActual; 
 
-generarQR();
-//codigo para generar QR , codificando el QR
-function  generarQR(){
-    alert("entrando  ala  funcion");
-    function htmlEncode(value){
-        return $('<div/>').text(value).html();
-      }
-      
-      $(function() {
-        $("#generate").click(function() {
-          $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
-        });
-      });      
+function obtenerHoraActual(){         
+var hoy = new Date();   
+    return hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();   
 }
+
+
+setInterval(    
+    function(){            
+            generar_qr_ala_hora("0:57:00", random(1,10000));        
+    }, 2000);
+
+    setInterval(    
+        function(){                
+                generar_qr_ala_hora("0:59:00", random(1,10000));            
+        }, 2000);
+        
+ 
+    
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+var contadorHora = 0;
+var horaReferencia = "12:00:00";
+var puedoEntrar = true;
+function generar_qr_ala_hora(pHora, numeroRandom){      
+    //rango de  hora
+    //alert(numeroRandom);
+    var rangoMaxHora;    
+    rangoMaxHora  = pHora.slice(0, -3);
+    rangoMaxHora = rangoMaxHora + ':59';
+      //alert("hora actual " + obtenerHoraActual() + " phora "+ pHora + " rango max:"+rangoMaxHora);
+    if(obtenerHoraActual() >= pHora && obtenerHoraActual() < rangoMaxHora && horaReferencia != pHora){        
+        //insertamos datos en el imnput         
+        document.getElementById("content").value = numeroRandom;        
+        generarQR();             
+        horaReferencia = pHora; 
+    }else{     
+        //alert("No entra");                   
+    }
+}
+
+//generarQR();
+//codigo para generar QR , codificando el QR
+    
+function htmlEncode(value){
+    return $('<div/>').text(value).html();
+}    
+
+$(function() {
+    $("#generate").click(function() {
+    document.getElementById("content").value =  random(1,10000);        
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
+    });
+});  
+
+function generarQR(){   
+    alert("generando");
+    $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
+}
+
