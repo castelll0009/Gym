10d1c2ccd99a4853f8e853892d20e6185a736b58
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-01-09 20:57:47 -0500

    agregando codigo para  generar QRs

--- js/QR.js
+++ js/QR.js
@@ -1,3 +1,5 @@
+//Generar QR  con nodejs
+/* 
 const fs = require("fs"); //acedemos  a files sistem
 const qrcode = require("qrcode"); //modulo para generar QR
 
@@ -10,3 +12,18 @@ const  run = async() => {
     fs.writeFileSync('./index-prueba.html', `${htmlContent}`)    
 }
 run()
+*/
+alert("entrando  ala  funcion");
+//codigo para generar QR , codificando el QR
+function  generarQR(){
+    alert("entrando  ala  funcion");
+    function htmlEncode(value){
+        return $('<div/>').text(value).html();
+      }
+      
+      $(function() {
+        $("#generate").click(function() {
+          $(".qr-code").attr("src", "https://chart.googleapis.com/chart?cht=qr&chl=" + htmlEncode($("#content").val()) + "&chs=160x160&chld=L|0");
+        });
+      });      
+}
