54652d5f0e14ecbee439e7a218da259a8cabc09b
MedardoQ <medardoquinonez1724@gmail.com>
2022-01-25 16:30:21 -0500

    circulo de progreso
    realizamos la implementacion del el circulo de progreso en la interfaz  de escanear qr y hay una barra en la que se escribe el porcentaje que se requiere.
    archivos agregados
    circulo-progreso.js
    y se modifico escanear-QR.css,interfaz_cliente.html

---
+++ js/circulo-progreso.js
@@ -0,0 +1,29 @@
+var input_porcentaje=document.querySelector("#percent");
+input_porcentaje.addEventListener("click",function(){
+    event.stopPropagation();
+});
+
+
+
+
+$('#percent').on('change', function(){
+    var val = parseInt($(this).val());
+    var $circle = $('#svg #bar');
+    
+    if (isNaN(val)) {
+     val = 100; 
+    }
+    else{
+      var r = $circle.attr('r');
+      var c = Math.PI*(r*2);
+     
+      if (val < 0) { val = 0;}
+      if (val > 100) { val = 100;}
+      
+      var pct = ((100-val)/100)*c;
+      
+      $circle.css({ strokeDashoffset: pct});
+      
+      $('#cont').attr('data-pct',val);
+    }
+  });
